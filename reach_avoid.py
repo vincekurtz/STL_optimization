29c9edc5d60d08eae49f9dba30e36f5a4c0ad2d0
Vince Kurtz <vjkurtz@gmail.com>
2019-02-07 17:07:40 -0500

    update example with control constraints and Nelder-Mead optimization

--- reach_avoid.py
+++ reach_avoid.py
@@ -7,6 +7,7 @@
 #
 ##
 
+import time
 import numpy as np
 from pySTL import STLFormula
 import matplotlib.pyplot as plt
@@ -42,7 +43,8 @@ ax.add_patch(target)
 def signal(x_0,u):
     """
     Maps a control signal u to an STL signal we can check. Here we consider
-    the signal to be the (x,y) position of a robot with double integrator dynamics. 
+    a robot with double integrator dynamics. The signal we will check is 
+    composed of the (x,y) position of the robot and the control inputs.
 
     Arguments:
         x_0 : a (n,1) numpy array representing the system's initial state
@@ -55,13 +57,14 @@ def signal(x_0,u):
     T = u.shape[1]      # number of timesteps
 
     # Pre-alocate the signal
-    s = np.zeros((2,T)) 
+    s = np.zeros((4,T)) 
 
     # Run the controls through the system and see what we get
     x = x_0
     for t in range(T):
         # extract the first and third elements of x
-        s[:,t] = x[[0,2],:].flatten()   
+        s[0:2,t] = x[[0,2],:].flatten()   
+        s[2:4,t] = u[:,t]
 
         # Update the system state
         x = A@x + B@u[:,t][:,np.newaxis]   # ensure u is of shape (2,1) before applying
@@ -75,8 +78,9 @@ def signal(x_0,u):
 
 # Now we define the property we're interested in:
 #   ALWAYS don't hit the obstacle and EVENTUALLY reach the goal in 20 steps.
+Nsteps = 20
 
-# We'll assume the signal that our specification is over is a list of x,y coordinates
+# Our specification is over is a list of x,y coordinates and the control inputs u
 # at each timestep. We'll build up our specification from predicates, with the use of 
 # this handy helper function:
 
@@ -104,10 +108,24 @@ def in_rectangle_formula(xmin,xmax,ymin,ymax):
 hit_obstacle = in_rectangle_formula(3,5,4,6)
 at_goal = in_rectangle_formula(7,8,8,9)
 
-obstacle_avoidance = hit_obstacle.negation().always(0,20)
-reach_goal = at_goal.eventually(0,20)
+obstacle_avoidance = hit_obstacle.negation().always(0,Nsteps)
+reach_goal = at_goal.eventually(0,Nsteps)
 
-full_specification = obstacle_avoidance.conjunction(reach_goal)
+# We'll similarly define constraints on the controls
+umin = -0.2
+umax = 0.2
+u1_above_min = STLFormula(lambda s, t : s[t,2] - umin)
+u1_below_max = STLFormula(lambda s, t : -s[t,2] + umax)
+u2_above_min = STLFormula(lambda s, t : s[t,3] - umin)
+u2_below_max = STLFormula(lambda s, t : -s[t,3] + umax)
+
+u1_valid = u1_above_min.conjunction(u1_below_max)
+u2_valid = u2_above_min.conjunction(u2_below_max)
+
+control_bounded = u1_valid.conjunction(u2_valid).always(0,Nsteps)
+
+# Finally, we can put these together for the full specification
+full_specification = obstacle_avoidance.conjunction(reach_goal).conjunction(control_bounded)
 
 ###########################################################################
 # Optimization                                                            #
@@ -142,10 +160,6 @@ def cost_function(u, m, x_0, specification, signal):
 
     return J
 
-# Now we'll use scipy to optimize this cost function: any one of a variety of
-# methods might be used
-
-
 # Some parameters
 x_0 = np.array([0,0,0,0])[:,np.newaxis]
 m = 2
@@ -154,21 +168,34 @@ T = 21
 # initial guess
 u_0 = np.zeros((m,T)).flatten()  # initial guess
 
-# Run the minimization!
+# Now we'll use scipy to optimize this cost function: any one of a variety of
+# methods might be used
+print("Optimizing with Adaptive Nelder-Mead...")
+start_time = time.time()
 res = minimize(cost_function, u_0, 
         args=(m, x_0, full_specification, signal),
-        method='Nelder-Mead')
-
-print(res)
+        method='Nelder-Mead',
+        options={'maxiter':25000,'disp':True,'adaptive':True,'fatol':1e-8,'xatol':1e-8})
+end_time = time.time()
 
 u_star = res.x.reshape(m,T)
 
-rho1 = cost_function(u_star.flatten(), 2, x_0, full_specification, signal)
-print(rho1)
+print(res)
+
+print("")
+print("Computation Time: %0.5f" % (end_time-start_time))
 
 ###########################################################################
 # Evaluating Optimal Trajectory                                           #
 ###########################################################################
+
+# We can evaluate how well the controller performs on our various requirements
+print("")
+print("Total Robustness Score: %0.5fs" % full_specification.robustness(signal(x_0,u_star).T,0))
+print("Control: %0.5f" % control_bounded.robustness(signal(x_0,u_star).T,0))
+print("Obstacle Avoidance: %0.5f" % obstacle_avoidance.robustness(signal(x_0,u_star).T,0))
+print("Goal Reaching: %0.5f" % reach_goal.robustness(signal(x_0,u_star).T,0))
+print("")
 
 # Finally, we'll plot the optimal trajectory to see how we did
 
