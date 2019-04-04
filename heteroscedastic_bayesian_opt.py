f17b9db57f2c0da1cab86b0246b6aa78117e11c4
Vince Kurtz <vjkurtz@gmail.com>
2019-04-04 14:58:23 -0400

    fix LCB and improve simple example

--- heteroscedastic_bayesian_opt.py
+++ heteroscedastic_bayesian_opt.py
@@ -19,7 +19,7 @@ def LCB(model, X, beta=1):
     at position X.
     """
     mu, sigma = model.predict_noiseless(X[:,None])
-    lcb = mu + beta*sigma
+    lcb = mu - beta*sigma
     return lcb
 
 def get_next_sample(model, X, bounds, n_restarts=25):
@@ -31,7 +31,7 @@ def get_next_sample(model, X, bounds, n_restarts=25):
     min_x = None
 
     def min_obj(X):
-        return LCB(model, X, beta=0.5)
+        return LCB(model, X, beta=2.5)
 
     for x0 in np.random.uniform(bounds[:,0], bounds[:,1], size=(n_restarts, dim)):
         res = minimize(min_obj, x0=x0, bounds=bounds, method='L-BFGS-B')
@@ -45,11 +45,17 @@ def get_next_sample(model, X, bounds, n_restarts=25):
 def f(X):
     return np.sin(3*X) + X**2 - 0.7*X
 
+# Bounds on the input space
 bounds = np.asarray([[-1.0,2.0]])
 
+state_dependent_noise = True  # For testing
+
 # Take initial samples and record the associated uncertainty
-X = np.random.uniform(-1,2,2)[:,None]
-error = np.random.normal(0,0.2,X.size)[:,None]
+X = np.random.uniform(-1,2,5)[:,None]
+if state_dependent_noise:
+    error = 0.4-0.3*X
+else:
+    error = 0.2+ np.zeros(X.size)[:,None]
 Y = f(X) + error
 
 # Set up some plots
@@ -64,21 +70,23 @@ for i in range(n_iter):
     # Use the (known) uncertainty on each observation
     m['.*het_Gauss.variance'] = abs(error) #Set the noise parameters to the error in Y
     m.het_Gauss.variance.fix() #We can fix the noise term, since we already know it
-    #m.optimize()
+    m.optimize()
 
     # Get the next sampling point
-    #X_next = np.asarray([np.random.uniform(-1,2)])
     X_next = get_next_sample(m,X,bounds)
 
     # Get the next sample and uncertainty estimate
-    error_next = np.random.normal(0,0.2,1)
+    if state_dependent_noise:
+        error_next = 0.4-0.3*X_next
+    else:
+        error_next = 0.2
     Y_next = f(X_next) + error_next
 
     # Plot the estimate so far, along with the next point we'll sample
     ax[0].clear()
     m.plot_f(ax=ax[0])
     ax[0].plot(X,Y,'kx',mew=1.5)
-    ax[0].errorbar(X,Y,yerr=np.array(m.likelihood.flattened_parameters).flatten(),fmt="none",ecolor='k',zorder=1)
+    ax[0].errorbar(X,Y,yerr=abs(error.flatten()),fmt="none",ecolor='k',zorder=1)
     ax[0].plot(np.linspace(-1,2),f(np.linspace(-1,2)),'r-',label="True f(x)")
     ax[0].axvline(x=X_next, label="Next Sample", color='k', linestyle='--')
     ax[0].set_ylim((-2,2))
@@ -94,7 +102,6 @@ for i in range(n_iter):
     plt.pause(0.01)
 
     raw_input("Press [enter] to continue ")
-
 
     # Append these new values to the data
     X = np.vstack([X_next,X])
